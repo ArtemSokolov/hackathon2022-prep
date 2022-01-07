ae0e7037c26c8e76a3b4b3e31fcfd40f2b8e154c
Artem Sokolov <artem.sokolov@gmail.com>
2022-01-07 15:12:21 -0500

    Optimal leaf ordering

--- 01-registration.R
+++ 01-registration.R
@@ -1,4 +1,10 @@
 library(tidyverse)
+library(seriation)
+
+## Intereset level (il)
+il <- c("Very interested"     = 3,
+        "Somewhat interested" = 2,
+        "Not interested"      = 1)
 
 ## Challenge tags (ct)
 ct <- c("01-artifacts",
@@ -24,8 +30,27 @@ sn <- set_names(10:21, ct) %>%
       Languages   = 8,
       GPU         = 9)
 
-## Load all registrant information
-X <- read_csv('registration.csv', col_types=cols()) %>% rename(!!!sn)
+## Load and clean up all registrant information
+X <- read_csv('data/registration.csv', col_types=cols()) %>%
+    rename(!!!sn) %>%
+    mutate_at(ct, replace_na,  "Not interested")
+stopifnot(anyNA(X)==FALSE)
+write_csv(X, "data/reg-clean.csv")
+
+## Compute the distance matrix (DM) of challenge interest
+DM <- X %>% select(Name, all_of(ct)) %>%
+    mutate_at(ct, recode, !!!il) %>% as.data.frame() %>%
+    column_to_rownames("Name") %>% dist
+
+## Compute optimal leaf ordering (olo)
+olo <- hclust(DM) %>% reorder(DM) %>%
+    dendextend::order.hclust() %>% labels(DM)[.]
+
+## Overall view of registrant interest in the challenges
+Z <- X %>% select(Name, all_of(ct)) %>%
+    pivot_longer(-Name, names_to="Challenge", values_to="Interest") %>%
+    mutate(Name      = factor(Name, levels=olo),
+           Challenge = factor(Challenge, rev(ct)))
 
 ## Plot elements to remove (petr)
 petr <- c("axis.text.x",
@@ -34,9 +59,7 @@ petr <- c("axis.text.x",
           "panel.grid.minor") %>%
     set_names() %>% map(~element_blank())
 
-## Overall view of registrant interest in the challenges
-Z <- X %>% select(Name, all_of(ct)) %>%
-    pivot_longer(-Name, names_to="Challenge", values_to="Interest")
+## Overview plot
 gg1 <- ggplot(Z, aes(x=Name, y=Challenge, fill=Interest)) +
     geom_tile() + theme_minimal() + xlab("Registrant") +
     scale_fill_brewer(palette="Reds", na.value="gray90", na.translate=FALSE) +
