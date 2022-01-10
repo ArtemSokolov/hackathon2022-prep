6d660c74bff8a3ee5e84eaca91fdf8ad030d90ec
Artem Sokolov <artem.sokolov@gmail.com>
2022-01-10 16:12:14 -0500

    Additional constraint on the difference in the number of participants

---
+++ 01-assign.R
@@ -0,0 +1,110 @@
+library(tidyverse)
+
+## Input filename (ifn)
+ifn <- "data/CSBC_PS-ON Image Analysis Hackathon 2022 (Responses) - Form Responses 1.csv"
+
+## Intereset level (il)
+il <- c("Very interested"     = 3,
+        "Somewhat interested" = 2,
+        "Not interested"      = 1)
+
+## Challenge tags (ct)
+ct <- c("01-artifacts",
+        "02-segmentation",
+        "03-virtual-IF",
+        "04-VAE",
+        "05-cross-talk",
+        "06-viz-comp",
+        "07-TB-scale",
+        "08-thumbnails",
+        "09-galaxy",
+        "10-neuroglancer",
+        "11-endosomes",
+        "12-cosmetic")
+
+## Shorthand notations (sn) for column names
+sn <- set_names(10:21, ct) %>%
+    c(Email       = 2,
+      Institution = 4,
+      Stage       = 5,
+      Network     = 6,
+      Experience  = 7,
+      Languages   = 8,
+      GPU         = 9)
+
+## Load and clean up all registrant information
+X <- read_csv(ifn, col_types=cols()) %>%
+    rename(!!!sn) %>% select(-Timestamp) %>%
+    mutate(across(all_of(ct), replace_na,  "Not interested"))
+
+stopifnot(anyNA(X)==FALSE)
+nc <- length(ct)           # Number of challenges
+np <- nrow(X)              # Number of people
+
+## Maximize the interest of participants
+w <- X %>% select(all_of(ct)) %>%
+    mutate(across(everything(), recode, !!!il)) %>%
+    as.matrix() %>% c()
+
+## Such that...
+MAT <- list()
+DIR <- list()
+RHS <- list()
+
+## 1. Assignment is binary (yes=1, no=0)
+MAT[[1]] <- diag(np*nc)
+DIR[[1]] <- rep("<=", np*nc)
+RHS[[1]] <- rep(1, np*nc)
+
+## 2. Each person is assigned to at most one challenge
+MAT[[2]] <- kronecker( t(rep(1,nc)), diag(np) )
+DIR[[2]] <- rep("<=", np)
+RHS[[2]] <- rep(1, np)
+
+## 3. Each challenge has at least the average number of participants
+MAT[[3]] <- kronecker( diag(nc), t(rep(1,np)) )
+DIR[[3]] <- rep(">=", nc)
+RHS[[3]] <- rep(floor(np/nc), nc)
+
+## 4. The difference in the number of participants between any
+##    two challenges is not greater than 1
+MAT[[4]] <- map(1:nc, ~`[<-`(-diag(nc)[-.x,],,.x,1)) %>%
+    do.call(rbind, .) %>% kronecker( t(rep(1,np)) )
+DIR[[4]] <- rep("<=", nc*(nc-1))
+RHS[[4]] <- rep(1, nc*(nc-1))
+
+## 5. Each challenge includes at least one person with a GPU
+MAT[[5]] <- kronecker( diag(nc), t(as.integer(X$GPU == "Yes")) )
+DIR[[5]] <- rep(">=", nc)
+RHS[[5]] <- rep(1, nc)
+
+## 6. Each challenge includes adequate image analysis expertise
+MAT[[6]] <- kronecker( diag(nc), t(as.integer(X$Experience >= 4)) )
+DIR[[6]] <- rep(">=", nc)
+RHS[[6]] <- rep(1, nc)
+
+## Solve via integer programming
+Z <- lpSolve::lp(direction    = 'max',
+                 objective.in = w,
+                 const.mat    = do.call(rbind, MAT),
+                 const.dir    = do.call(c, DIR),
+                 const.rhs    = do.call(c, RHS),
+                 int.vec      = 1:(nc*np))
+z <- Z$solution
+
+## Quick checks
+w[z]
+matrix(z, np, nc) %>% rowSums()
+matrix(z, np, nc) %>% colSums()
+
+## Wrangle the solution and join against the participant list
+Y <- matrix(z, np, nc) %>% apply(1, which.max) %>% ct[.] %>%
+    mutate( X, Assignment = . )
+
+## Additional checks
+Y %>% group_by(Assignment) %>%
+    summarize(nGPU    = sum(GPU == "Yes"),
+              nExpert = sum(Experience >= 4))
+
+Y %>% select( Assignment, Name, Email, everything() ) %>%
+    write_csv("data/assignment.csv")
