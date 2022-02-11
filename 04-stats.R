1c879e385d485a59910b3b44343cb1584e38ac1d
Artem Sokolov <artem.sokolov@gmail.com>
2022-02-11 14:34:07 -0500

    Updated stats

--- 04-stats.R
+++ 04-stats.R
@@ -22,12 +22,12 @@ ggsave("plots/04-stage.png", gg1, width=10, height=4)
 
 ## Experience Level
 gg2 <- ggplot(X, aes(x=Experience, fill=GPU)) +
-    geom_bar() +
+    geom_bar() + ggtitle(str_c(nrow(X), " registrants")) +
     theme_minimal() + coord_flip() +
     xlab("Experience Level") + ylab("") +
     scale_fill_manual(values=c(Yes='tomato', No='steelblue')) +
     theme(axis.title.y = etxt(14), axis.title.x = eblk(), axis.text = etxt(12),
-          legend.title = etxt(14), legend.text = etxt(12),
+          legend.title = etxt(14), legend.text = etxt(12), title=etxt(14),
           panel.grid.major.y = eblk(), panel.grid.minor.y = eblk())
     
 ggsave("plots/04-experience.png", gg2, width=7, height=4)
@@ -42,3 +42,16 @@ pdf("plots/04-languages.pdf", width=7, height=3)
 upset(fromExpression(vCount), order.by='freq', mb.ratio=c(0.70, 0.30))
 dev.off()
 
+## Most-represented Institutions
+Y <- X %>% count(Institution) %>%
+    filter(n > 1) %>% arrange(n) %>%
+    mutate(across(Institution, factor, Institution, Institution))
+gg3 <- ggplot(Y, aes(x=Institution, y=n)) +
+    geom_bar(stat='identity') + ylab("# Registrants") +
+    theme_minimal() + coord_flip() +
+    scale_y_continuous(breaks=seq(2,10,by=2)) +
+    theme(axis.title.y = eblk(), axis.text = etxt(12),
+          panel.grid.major.y = eblk(), panel.grid.minor.y = eblk(),
+          panel.grid.minor.x = eblk())
+
+ggsave("plots/04-inst.png", gg3, width=7, height=4)
